e5085ccd273b01725f8f918a786316877dab60e7
Maxime Filippini <maxime.filippini@gmail.com>
2024-11-14 18:46:48 +0100

    fix ptf page

--- src/routes/portfolios/+page.server.ts
+++ src/routes/portfolios/+page.server.ts
@@ -1,15 +1,8 @@
-import { db } from '@/server/db';
 import type { PageServerLoad } from './$types';
-import { portfolio, user } from '@/server/db/schema';
-import { eq } from 'drizzle-orm';
 
 export const load = (async () => {
-	const query = db.select().from(portfolio).innerJoin(user, eq(portfolio.userId, user.id));
-
-	const res = await query;
-
 	return {
-		portfolios: res,
-		nPtf: res.length
+		portfolios: [],
+		nPtf: 0
 	};
 }) satisfies PageServerLoad;
